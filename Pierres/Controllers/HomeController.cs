690fe70ba9c04084831da29134561cfba924a8c1
Keisha Hepner <kmhepner90@gmail.com>
2022-10-23 00:03:24 -0700

    update Home index view and home controller to fix connection issues

--- Pierres/Controllers/HomeController.cs
+++ Pierres/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Pierres.Models;
 
 namespace Pierres.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresContext _db;
 
-    [HttpGet("/")]
+    public HomeController(PierresContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      return View();
+      ViewBag.Flavor = new List<Flavor>(_db.Flavors);
+      return View(_db.Treats.ToList());
     }
   }
 }
